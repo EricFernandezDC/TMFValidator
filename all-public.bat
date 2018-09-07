6622afd206e722e7fbbece29eb2b8277489c2fc6
sharrop <harrop.home@gmail.com>
2018-09-07 16:50:22 +0100

    Updated to include every published API (Apache and RAND).

--- all-public.bat
+++ all-public.bat
@@ -34,4 +34,29 @@ py validator.py -s -l -d ^
     https://raw.githubusercontent.com/tmforum-apis/TMF675_GeographicLocation/master/GeographicLocation.regular.swagger.json ^
     https://raw.githubusercontent.com/tmforum-apis/TMF673_GeographicAddress/master/GeographicAddress_Management.regular.swagger.json ^
     https://raw.githubusercontent.com/tmforum-apis/TMF648_QuoteManagement/master/Quote_Management.regular.swagger.json ^
-    https://raw.githubusercontent.com/tmforum-apis/TMF620_ProductCatalog/master/TMF620_Product_Catalog_Management.regular.swagger.json
+    https://raw.githubusercontent.com/tmforum-apis/TMF620_ProductCatalog/master/TMF620_Product_Catalog_Management.regular.swagger.json ^
+    https://raw.githubusercontent.com/tmforum/TMFAPISWAGGER/develop/serviceActivationAndConfiguration/v2/swagger2.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF641_ServiceOrder/master/Service_Ordering_Management.regular.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF633_ServiceCatalog/master/TMF633_Service_Catalog_Management.admin.swagger_R17.5.json ^
+    https://raw.githubusercontent.com/tmforum/TMFAPISWAGGER/develop/alarmManagement/v3/swagger2.json ^
+    https://raw.githubusercontent.com/tmforum/TMFAPISWAGGER/develop/documentManagement/v2/swagger2.json ^
+    https://raw.githubusercontent.com/tmforum/TMFAPISWAGGER/develop/entityCatalogManagement/v3/swagger2.json ^
+    https://raw.githubusercontent.com/tmforum/DSPRIVACY/master/tmforum-Privacy-1.0-swagger-3.json ^
+    https://raw.githubusercontent.com/tmforum-apis/WIP_TMF664_ResourceFunctionActivationConfiguration/master/ResourceFunctionActivationAndConfiguration.swagger.1.5.0.json ^
+    https://raw.githubusercontent.com/tmforum-apis/WIP_TMF656_ServiceProblemManagement/master/Service_Problem_Management.admin.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/WIP_TMF623_SLAManagement/master/SLAManagement.swagger.2.0.json ^
+    https://raw.githubusercontent.com/tmforum/DSPRIVACY/master/tmforum-Privacy-1.0-swagger-3.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF658_LoyaltyManagement/master/Loyalty_Management.admin.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF683_PartyInteraction/master/partyInteractionManagement_TMF_v1.0.0Review2.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF676_PaymentManagement/master/paymentManagementTMF_v1.0.0_review9 ^
+    https://raw.githubusercontent.com/tmforum-apis/WIP_TMF649_PerformanceManagementThresholding/master/Perfromance_Management_Threshold.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF654_PrepayBalanceManagement/master/PrepayBalanceManagement_R17_v204.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF637_ProductInventory/master/Product_Inventory_Management.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF634_ResourceCatalog/master/TMF634_Resource_Catalog_Management.admin.swagger-R17.0.0.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF639_ResourceInventory/master/Resource_Inventory_Management.admin.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF652_ResourceOrderManagement/master/Resource_Ordering_Management.admin.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF685_ResourcePool/master/Resource_Pool_Management.admin.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF638_ServiceInventory/master/Service_Inventory_Management.regular.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF684_ShipmentTracking/master/shipmentTracking_v10_Jan18_SwaggerValidatorPassed.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF663_ShoppingCart/master/Shopping_Cart_Management.admin.swagger.json ^
+    https://raw.githubusercontent.com/tmforum-apis/TMF621_TroubleTicket/master/Trouble_Ticket.swagger.json 
